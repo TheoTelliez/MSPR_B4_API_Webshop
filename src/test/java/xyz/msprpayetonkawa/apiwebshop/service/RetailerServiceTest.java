3643627ba0c3e61aba1f93281a1e128c09749375
Slycer <slycermusics@gmail.com>
2023-08-27 11:10:53 +0200

    fix retailerservice

--- src/test/java/xyz/msprpayetonkawa/apiwebshop/service/RetailerServiceTest.java
+++ src/test/java/xyz/msprpayetonkawa/apiwebshop/service/RetailerServiceTest.java
@@ -32,12 +32,18 @@ public class RetailerServiceTest {
     private RetailerRepository retailerRepository;
 
     private final Product product1 = new Product(1L,"uid-key","Name","Description",11.11f,new Retailer(), 1, "image","bleu");
+    private final Product product2 = new Product(2L,"other-uid-key","Other-Name","Other-Description",22.22f,new Retailer(), 2, "image","noir");
 
     private final Product product3 = new Product("New-Name","New-Description",33.33f,3);
 
     private final List<Product> ListProducts1 = Arrays.asList(product1,product3);
 
     private final Retailer retailer1 = new Retailer(1L,"retailer-uid-key","Name","email@company.com","pass","Admin",ListProducts1);
+    private final List<Product> ListProducts2 = Arrays.asList(product1, product2);
+
+    private final Retailer retailer2 = new Retailer(2L,"other-retailer-uid-key","Other Name","email@othercompany.com","otherpass","Admin",ListProducts2);
+
+    private final List<Retailer> retailers = Arrays.asList(retailer1,retailer2);
 
     @Mock
     private PasswordEncoder passwordEncoder;
@@ -51,11 +57,27 @@ public class RetailerServiceTest {
     }
 
     @Test
+    public void testFindAllRetailers(){
+        Mockito.when(retailerRepository.findAll()).thenReturn(retailers);
+        List<Retailer> result = retailerService.getRetailers();
+        assertEquals(retailers,result);
+    }
+
+    @Test
     public void testFindRetailerByUidThatExists(){
         String uid = "retailer-uid-key";
         Mockito.when(retailerRepository.findByUid(uid)).thenReturn(retailer1);
         Retailer result = retailerService.getRetailerByUid(uid);
         assertEquals(retailer1,result);
     }
+
+    @Test
+    public void testFindRetailerByUidThatDoesNotExists(){
+        String uid = "no-uid-key";
+        Mockito.when(retailerRepository.findByUid(uid)).thenReturn(null);
+        Retailer result = retailerService.getRetailerByUid(uid);
+        assertNull(result);
+    }
+
 
 }
